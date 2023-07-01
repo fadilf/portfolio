7774cc1e34f6959777a0daa39b615b668f762061
Fadil Eledath <fadilfaizal@gmail.com>
2023-06-30 22:12:22 -0500

    Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import './globals.css'
-import { Inter, IBM_Plex_Sans } from 'next/font/google'
+import { IBM_Plex_Sans } from 'next/font/google'
 import NavBar from './NavBar'
 import clsx from 'clsx'
+import Script from 'next/script'
 
-// const inter = Inter({ subsets: ['latin'] })
+
 const ibmPlex = IBM_Plex_Sans({
 	subsets: ['latin'],
 	weight: ['100', '400', '500', '600', '700']
@@ -14,9 +15,27 @@ export default function RootLayout({
 }: {
 	children: React.ReactNode
 }) {
+
+	const GA_MEASUREMENT_ID = 'G-L536E2FBC2';
+
 	return (
 		<html lang="en" id="top">
-			<body className={clsx(ibmPlex.className, 'bg-gray-950','text-gray-300', 'font-normal', 'overflow-x-hidden')}>
+			<head>
+				<title>Fadil Eledath</title>
+				<Script
+					src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+					strategy="afterInteractive"
+				/>
+				<Script id="google-analytics" strategy="afterInteractive">
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){window.dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', '${GA_MEASUREMENT_ID}');
+					`}
+				</Script>
+			</head>
+			<body className={clsx(ibmPlex.className, 'bg-gray-950', 'text-gray-300', 'font-normal', 'overflow-x-hidden')}>
 				<NavBar />
 				<main>
 					{children}
